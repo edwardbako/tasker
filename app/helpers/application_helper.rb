98f1caa8c4e8356603cac3eebfcfa9f49ce9e6d1
Edward Bako <edward.bako@gmail.com>
2022-12-20 04:20:20 +0700

    Add basic layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def flash_class(name)
+        mapping = {
+          notice: :success,
+          alert: :warning,
+          error: :danger
+        }.with_indifferent_access
+        mapping.default = :info
+    
+        "alert-#{mapping[name]}"
+      end
 end
