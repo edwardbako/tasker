58cdd484b536e2277ac05529976c231a420b12cd
Edward Bako <edward.bako@gmail.com>
2022-12-23 15:08:01 +0700

    Seed database with some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: "p4ssw0rd"
+  )
+end
+
+users = User.all
+100.times do
+  task = Task.create!(
+    title: Faker::Hobby.activity,
+    deadline: Faker::Date.forward(days: 30),
+    owner: users.sample
+  )
+  task.state = rand(4)
+  task.completed_at = Faker::Date.forward(days: 30) if task.completed?
+  task.canceled_at = Faker::Date.forward(days: 30) if task.canceled?
+  task.save(validate: false)
+end
+
+Task.all.each do |task|
+  rand(10).times do |n|
+    approver = users.sample
+    unless approver.in? task.approvers
+      task.approvers << approver if approver != task.owner
+    end
+  end
+end
